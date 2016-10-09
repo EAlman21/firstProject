c1113ee755d7dcda3f2d5c1c3c2525106d33cd79
ealmanz0821 <enmana21@gmail.com>
2016-10-09 13:51:05 -0400

    modified gitignore package.json and add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+      concat:{
+        html:{
+          src:['index.html'],
+          dest:'build/built.html'
+        }
+      },
+      watch:{
+        html:{
+          files:['*.html'],
+          tasks:['concat:html']
+        }
+      }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['concat','watch'])
+}
