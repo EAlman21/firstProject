0b90f0366966aa5ae374e40a55890bdef7707c3c
ealmanz0821 <enmana21@gmail.com>
2016-10-18 17:36:34 -0400

    fixed navbar to be align in both html pages

--- js/bookBay.js
+++ js/bookBay.js
@@ -2,5 +2,23 @@ var app = angular.module('bookBayApp',[]);
 
 //add definition to sign up in authentication page
 app.controller('authController',function($scope){
+  $scope.user = {username:'', password:''};
+  $scope.error_msg = '';
+
+  $scope.login = function(){
+    //need to finish this
+    $scope.error_msg = 'login request for '+ $scope.user.username;
+  }
+  $scope.register = function(){
+    $scope.error_msg = 'registeration request for '+ $scope.user.username +' is taken';
+  }
+
+});
+
+app.controller('inboxContoller',function(){
+
+});
+
+app.controller('settingController',function(){
 
 });
