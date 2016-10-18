ec7e695a65e8bbf0b7a0b2afb799daf43c7aa3ce
ealmanz0821 <enmana21@gmail.com>
2016-10-18 15:06:44 -0400

    added angular on the authentication page

---
+++ js/bookBay.js
@@ -0,0 +1,11 @@
+var app = angular.module('bookBayApp',[]);
+
+//add definition to sign up in authentication page
+app.controller('signInController',function($scope){
+
+});
+
+//add defintion to sign up in authentication page
+app.controller('signUpController',function($scope){
+  
+});
